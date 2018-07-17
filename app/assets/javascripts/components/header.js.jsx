adcd983171403a575fb4e567be189b630aa86fa9
robertvilvert <robert.lvilvert@gmail.com>
2018-07-17 09:24:53 -0300

    Add navbar using react

---
+++ app/assets/javascripts/components/header.js.jsx
@@ -0,0 +1,29 @@
+class Header extends React.Component {
+
+    render(){
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                            <span className="sr-only"> </span>
+                            <span className="icon-bar"> </span>
+                            <span className="icon-bar"> </span>
+                            <span className="icon-bar"> </span>
+                        </button>
+                        <a href="" className="navbar-brand">App</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbar">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </nav>
+        )
+    }
+
+}
--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -2,6 +2,7 @@ class Main extends React.Component {
     render(){
         return (
             <div>
+                <Header />
                 <h1>Hello From React Rails</h1>
             </div>
         )
